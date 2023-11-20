b31ee9065b9ec4c764d256a8f6c909e58f04996e
jayshri_b <146444989+jayshrivrdella@users.noreply.github.com>
2023-11-20 14:49:53 +0530

    Update urls.py

--- knox_auth_pro/knox_auth_app/urls.py
+++ knox_auth_pro/knox_auth_app/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('api/register/', RegisterAPI.as_view(), name='register'),
     path('api/logout/', knox_views.LogoutView.as_view(), name="logout"),
     path('api/login/', LoginAPI.as_view(), name="login"),
-    path('api/logout/', knox_views.LogoutView.as_view(),name="logout"),
     path('api/logoutall/', knox_views.LogoutAllView.as_view(),name="logoutall"),
     path('api/updatepassword/', ChangePasswordView.as_view(), name="change-password"),
 ]
